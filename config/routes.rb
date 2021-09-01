b4e0dadac24f89a7c3185487578117db3c068b86
zdurr22 <zdurr22@gmail.com>
2021-09-01 12:03:01 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'secrets/new'
+
+  root 'application#welcome'
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
+  get '/secret' => 'secrets#show'
+
+
 end
